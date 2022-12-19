b75e41210349eb0fbe890c6213c0dde85b05b69a
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2022-12-19 19:16:55 -0300

    clase21/22inicio

--- src/main/java/ar/com/practica/Controllers/HomeController.java
+++ src/main/java/ar/com/practica/Controllers/HomeController.java
@@ -1,10 +1,7 @@
 package ar.com.practica.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import ar.com.practica.dominian.User;
 
 
 
---
+++ src/main/java/ar/com/practica/dominian/Cupones.java
@@ -0,0 +1,17 @@
+package ar.com.practica.dominian;
+
+import java.util.Date;
+
+import lombok.Data;
+
+
+@Data
+public class Cupones {
+	private Long id;
+	private String nombre;
+	private String codigo;
+	private Date fechaVigenciaDesde;
+	private Date fechaVigenciaHasta;
+	private Float descuento;
+	
+}
